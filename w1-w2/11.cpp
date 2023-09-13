94a2bfb90c8c03252385a4770c115a2ba733f609
Nariman Ganiyev <narimanganiyev26@gmail.com>
2023-09-13 15:35:10 +0600

    w2

--- w1-w2/11.cpp
+++ w1-w2/11.cpp
@@ -1,10 +1,34 @@
-// Node
+// Node object
+// queue [1,2,3,4,5]
+// node1 -> data
+// node1 => data->1
+// node2 => data->2
 
+// node1->next = node2->data
+// node1->next->next = node3
 
-// a = [1,2,3,4,5]
-// 1 2 3 4 5 node
-// node 1
-// node.next 2
-// node.next.next.prev 2
+#include <iostream>
 
-// pop_front node = node.next prev
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+    Node();
+};
+
+int main () {
+    Node *node1 = new Node(6);
+    Node *node2;
+    node2->data = 5;
+
+    node1->next = node2;
+    
+    cout<<node1->next->data;
+}
